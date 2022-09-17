b3652fc95d9098cd09a0341988ed46a7f9f5f934
letoro99 <atgmatias.12@gmail.com>
2022-09-17 18:06:46 -0400

    add plot for algortimo3

--- src/Main.java
+++ src/Main.java
@@ -8,6 +8,7 @@ public class Main {
         System.out.println("Hello, World!");
         Algoritmo1 algoritmo1 = new Algoritmo1();
         Algoritmo2 algoritmo2 = new Algoritmo2();
+        Algoritmo3 algoritmo3 = new Algoritmo3();
 
         Plot plot = new Plot("Título", "xLabel", "yLabel");
 
@@ -27,6 +28,12 @@ public class Main {
             alg2[j] = (int) Utils.promedio(50, x[j], algoritmo2);
         }
         plot.addSerie("Algoritmo 2", x, alg2);
+
+        int[] alg3 = new int[11];
+        for (int j=0; j<x.length; j++) {
+            alg3[j] = (int) Utils.promedio(50, x[j], algoritmo3);
+        }
+        plot.addSerie("Algoritmo 3", x, alg3);
         
         plot.finish();
 
